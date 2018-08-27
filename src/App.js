715fb6ae9c1af943a5918a6b87f7aadc3e14f035
Thangesh <thangeswaran.kasirajan@wipro.com>
2018-08-27 21:47:01 +1000

    add ui

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TableStatistics from './components/TableStatistics'
+import ChartStatistics from './components/ChartStatistics'
+
+class App extends Component {
+  render() {
+    return (
+    	<div>
+      <TableStatistics />
+      <ChartStatistics />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TableStatistics.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+
+class ChartStatistics extends Component {
+  render() {
+    return (
+      <div>
+     <table>
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td> 
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td> 
+    <td>94</td>
+  </tr>
+</table>
+      </div>
+    );
+  }
+}
+
+export default ChartStatistics;
